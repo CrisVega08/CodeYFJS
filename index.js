f8468264832b2bae6640b4f97bac72d3c73a38c8
León Ceballos <leon.ceballos@wolox.co>
2019-07-04 19:56:14 -0500

    Initial problem description

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const products = require("./products");
 const cant = require("./quantity");
 
-console.log("products", products);
-console.log("cant", cant);
+/**
+ * You will need make the variables and functions for buying products in the CYF store.
+ * At the end you the program will print:
+ * - Parcial total
+ * - Total
+ * - Products bought
+ **/
+
+
+
+
+
+
+
+
