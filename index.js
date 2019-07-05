9b1d14cd2ae8cc524c7b6d6018ccd5d263057ffe
León Ceballos <leon.ceballos@wolox.co>
2019-07-04 20:49:04 -0500

    Description of the problem

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const products = require("./products");
-const cant = require("./quantity");
+const quantity = require("./quantity");
+const clients = require("./clients");
 
 /**
  * You will need make the variables and functions for buying products in the CYF store.
- * At the end you the program will print:
- * - Parcial total
- * - Total
- * - Products bought
+ * Clients discounts:
+ * León Ceballos: 5%
+ * Cristian Vega: 10%
+ * Jesse Cogollo: 2%
  **/
 
+const productsList = [];
+const productsCant = [];
+
+/** 
+ * This fucntion has to return the total, fallowing the next rules:
+ * 1: Into the function you have to print in the console the list of the products and the quantity after you apply the buy.
+ * 2: The sum of all the products cost with 2 decimals.
+ * 3: The total of the discount.
+ **/
+const result = getCostList(clients[0], productsList, productsCant);
 
 
-
-
-
-
-
+console.log('The total of you purchase is: ' + result)
