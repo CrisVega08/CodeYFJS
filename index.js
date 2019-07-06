db91395cc51311601e95d94ea5ae03d8a36406ad
León Ceballos <leon.ceballos@wolox.co>
2019-07-05 19:52:49 -0500

    Add initial arrays

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ const clients = require("./clients");
  * Jesse Cogollo: 2%
  **/
 
-const productsList = [];
-const productsCant = [];
+const productsList = [34, 1, 9, 8, 23, 11, 40, 14, 6, 33, 29, 20, 41, 15, 5];
+const productsCant = [2, 5, 3, 9, 2, 4, 2, 3, 10, 8, 6, 12, 4, 5, 2, 7, 7];
 
 /** 
- * Restricciones:
- * 1: Dentro de la función principal deberás imprimir la lista de productos con la cantidad con la que quedaron después de la compra.
- * 2: El valor total sin aplicar el descuento. El número debe tener dos decimales.
- * 3: El total del descuento aplicado.
+ * Requerimientos:
+ * 1: Dentro de la función principal deberás imprimir la lista de productos con la cantidad restante después de la compra.
+ * 2: Imprimir el valor total sin aplicar el descuento. El número debe tener dos decimales.
+ * 3: Imprimir el total del descuento aplicado.
+ * 4: Si no hay suficientes productos se debe agregar a la lista de compra solo los que alcancen.
  **/
 
 // TODO: Si crees que es necesesario incluye más funciones para las pequeñas tareas
