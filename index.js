60c318e21e42e4038f841730d078577e2b761b39
León Ceballos <leon.ceballos@wolox.co>
2019-07-05 20:18:12 -0500

    Additional instructions

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const products = require("./products");
 const quantity = require("./quantity");
 const clients = require("./clients");
+const costs = require("./costs");
 
 /**
  * Puedes crear las variables y funciones necesarias para que el sistema funcione.
- * Debe simular una compra en una tienda y arrojar el total de la compra teniendo en cuenta los siguiens descuentos:
+ * Debe simular una compra en una tienda y arrojar el total de la compra teniendo en cuenta los siguientes descuentos:
  * León Ceballos: 5%
  * Cristian Vega: 10%
  * Jesse Cogollo: 2%
  **/
 
-const productsList = [34, 1, 9, 8, 23, 11, 40, 14, 6, 33, 29, 20, 41, 15, 5];
-const productsCant = [2, 5, 3, 9, 2, 4, 2, 3, 10, 8, 6, 12, 4, 5, 2, 7, 7];
+const productsListIds = [34, 1, 9, 8, 23, 11, 40, 14, 6, 33, 29, 20, 41, 15, 5];
+const productsQuantities = [2, 5, 3, 9, 2, 4, 2, 3, 10, 8, 6, 12, 4, 5, 2, 7, 7];
 
 /** 
  * Requerimientos:
@@ -23,10 +24,32 @@ const productsCant = [2, 5, 3, 9, 2, 4, 2, 3, 10, 8, 6, 12, 4, 5, 2, 7, 7];
 
 // TODO: Si crees que es necesesario incluye más funciones para las pequeñas tareas
 
+// Funciones adicionales aquí...
 
 // TODO: Función Principal getCostList
+function getCostList(client, productsIds, quantities) {
+  // Código aquí...
+}
 
-const result = getCostList(clients[0], productsList, productsCant);
+const result = getCostList(clients[0], productsListIds, productsQuantities);
 
 // Imprime el total de la compra
 console.log('El total de la compra es: ' + result)
+
+/**
+ * TE RECOMENDAMOS
+ * 1. Primero recorre, suma y multiplica los productos de acuerdo a la cantidad
+ * 2. Saca el descuento según el cliente
+ * 3. Aplica el descuento  al total
+ * 4. Al utilizar el console.log pon titulos a lo que imprimes
+ */
+
+/**
+ * LISTA DE CHEQUEO
+ * Calcular el total de la compra
+ * Imprimir los productos con la  cantidad restante luego de la compra
+ * Calcular el descuento
+ * Imprimir el descuento
+ * Aplicar descuento al total
+ * Imprime el total
+ */
