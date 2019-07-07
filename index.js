110801a2001ce66d795edb30e1d73bd6c96730a4
angel10050 <angelangel10050@gmail.com>
2019-07-07 17:24:34 -0500

    el primer commit

--- index.js
+++ index.js
@@ -5,18 +5,38 @@ const costs = require("./costs");
 
 /**
  * Puedes crear las variables y funciones necesarias para que el sistema funcione.
- * Debe simular una compra en una tienda y arrojar el total de la compra teniendo en cuenta los siguientes descuentos:
+ * Debe simular una compra en una tienda y arrojar el total de la compra teniendo en cuenta
+los siguientes descuentos:
  * León Ceballos: 5%
  * Cristian Vega: 10%
  * Jesse Cogollo: 2%
  **/
 
 const productsListIds = [34, 1, 9, 8, 23, 11, 40, 14, 6, 33, 29, 20, 41, 15, 5];
-const productsQuantities = [2, 5, 3, 9, 2, 4, 2, 3, 10, 8, 6, 12, 4, 5, 2, 7, 7];
+const productsQuantities = [
+  2,
+  5,
+  3,
+  9,
+  2,
+  4,
+  2,
+  3,
+  10,
+  8,
+  6,
+  12,
+  4,
+  5,
+  2,
+  7,
+  7
+];
 
 /** 
  * Requerimientos:
- * 1: Dentro de la función principal deberás imprimir la lista de productos con la cantidad restante después de la compra.
+ * 1: Dentro de la función principal deberás imprimir la lista de productos con la cantidad restante
+  después de la compra.
  * 2: Imprimir el valor total sin aplicar el descuento. El número debe tener dos decimales.
  * 3: Imprimir el total del descuento aplicado.
  * 4: Si no hay suficientes productos se debe agregar a la lista de compra solo los que alcancen.
@@ -28,13 +48,35 @@ const productsQuantities = [2, 5, 3, 9, 2, 4, 2, 3, 10, 8, 6, 12, 4, 5, 2, 7, 7]
 
 // TODO: Función Principal getCostList
 function getCostList(client, productsIds, quantities) {
-  // Código aquí...
+  console.log(productsIds, quantities);
+  var total = 0;
+  productsIds.forEach((element, index) => {
+    var subtotal;
+    var inventario = quantity[element];
+    var pedidousuario = quantities[index];
+    if (inventario >= pedidousuario) {
+      subtotal = pedidousuario * costs[element];
+    } else {
+      subtotal = inventario * costs[element];
+    }
+    console.log(subtotal);
+    total = subtotal + total;
+
+    if (client) {
+      total = total - 0.1;
+    } else if (client[1]) {
+      total = total - 0.5;
+    } else {
+      total = total - 0.2;
+    }
+  });
+  console.log(total);
 }
 
 const result = getCostList(clients[0], productsListIds, productsQuantities);
 
 // Imprime el total de la compra
-console.log('El total de la compra es: ' + result)
+console.log("El total de la compra es: " + result);
 
 /**
  * TE RECOMENDAMOS
